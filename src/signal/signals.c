c3d627a558571dc6e9370ce4ab82204dfd45fcb9
Mihai Rusu <mrusu@2-B-9.42heilbronn.de>
2024-07-09 14:48:38 +0200

    aded signal and name update.. now

--- src/signal/signals.c
+++ src/signal/signals.c
@@ -6,8 +6,32 @@
 /*   By: mrusu <mrusu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 13:22:29 by mrusu             #+#    #+#             */
-/*   Updated: 2024/07/05 13:22:39 by mrusu            ###   ########.fr       */
+/*   Updated: 2024/07/09 12:53:34 by mrusu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+void	handle_signals(int sig)
+{
+	if (sig == SIGINT)
+	{
+		printf("\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
+	else if (sig == SIGQUIT)
+	{
+		printf("\b\b \b\b");
+		rl_on_new_line();
+		rl_redisplay();
+	}
+	else if (sig == SIGTSTP)
+	{
+		printf("\nSIGTSTP received\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
+}
